edaf653f0b322f9b1293044a8c389aa406b20d8d
aminor32 <aminor32dh@gmail.com>
2022-12-10 21:25:31 +0900

    Fix reload error

--- src/utils/atom.ts
+++ src/utils/atom.ts
@@ -5,7 +5,7 @@ import { Result } from "@/utils/type";
 export const websocketAtom = atom<WebSocket>(
   new WebSocket(`ws://3.39.227.40/ws/${uuid()}`)
 );
-export const connectedAtom = atom<boolean>(false);
+export const connectedAtom = atom<boolean>(true);
 export const currentWordAtom = atom<string>("");
 export const resultsAtom = atom<Result[]>([]);
 export const answerAtom = atom<string>("");
